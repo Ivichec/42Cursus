d1a511204e60449fbbda62c0e0e8ce23d4f574dd
Ivan Checa Garcia <icheca-g@c2r9s6.42madrid.com>
2024-10-12 18:27:12 +0200

    Perfecto ahora

--- libft.h
+++ libft.h
@@ -6,15 +6,16 @@
 /*   By: icheca-g <icheca-g@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/29 19:28:43 by icheca-g          #+#    #+#             */
-/*   Updated: 2024/10/12 12:48:02 by icheca-g         ###   ########.fr       */
+/*   Updated: 2024/10/12 18:26:37 by icheca-g         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#define LIBFT_H
-#include <stdlib.h>
-#include <unistd.h>
-#include <stddef.h>
-#include <stdio.h>
+#ifndef LIBFT_H
+# define LIBFT_H
+# include <stdlib.h>
+# include <unistd.h>
+# include <stddef.h>
+# include <stdio.h>
 
 int				ft_atoi(const char *str);
 void			ft_bzero(void *s, size_t n);
@@ -50,3 +51,5 @@ char			*ft_strtrim(const char *s1, const char *set);
 char			*ft_substr(const char *s, unsigned int start, size_t len);
 int				ft_tolower(int c);
 int				ft_toupper(int c);
+
+#endif
